fadda12bc89f0e10367f68a105dd3b38c12756c4
VitalikFans <vitalikfans@protonmail.com>
2021-10-24 14:18:25 +0800

    1

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,8 @@
-importScripts("https://cdn.myun.info/workbox-v4.3.1/workbox-sw.js", "precache-manifest.6cb81aedd42577a1e3eeeb567d4df030.js");
-
-// workbox.precaching([
-//     // 注册成功后要立即缓存的资源列表
-// ])
-// //
-
-// //
-
-// self.__precacheManifest = [].concat(self.__precacheManifest || []);
-// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+importScripts("https://cdn.myun.info/workbox-v4.3.1/workbox-sw.js", "precache-manifest.c9b3fee0fc5a3a09118f5b0600b4bf4a.js");
 
 
-
-// vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
-//
-workbox.core.skipWaiting(); // 强制等待中的 Service Worker 被激活
-workbox.core.clientsClaim(); // Service Worker 被激活后使其立即获得页面控制权
+workbox.core.skipWaiting(); 
+workbox.core.clientsClaim(); 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
 workbox.routing.registerRoute(
